fab43deec539d5cbcf6fecc270caab03f008fb33
Rafia-Shahbaz <rafiashahbaz34@gmail.com>
2025-07-21 15:42:35 +0500

    Add my-project

--- project4.py
+++ project4.py
@@ -9,3 +9,49 @@
 #Use math.sqrt() for numeric inputs
 #Use random.randint() to inject a test case
 #Use datetime.now() to timestamp the result
+import math
+import random
+from datetime import datetime
+def classify_type(item):
+    if isinstance(item, (int, float)):
+        return "numeric"
+    elif isinstance(item, str):
+        if item.lower() in ['true', 'false']:
+            return "other"
+        return "text"
+    else:
+        return "other"
+def cast_and_operate(item):
+        if isinstance(item, (int, float)):
+            result = item * 2
+            sqrt_val = round(math.sqrt(item), 2) if item >= 0 else None
+            return {"value": result, "sqrt": sqrt_val}
+        elif isinstance(item, str):
+            if item.lower() == "true":
+                return {"value": True}
+            elif item.lower() == "false":
+                return {"value": False}
+            else:
+                return {"value": item.upper()}
+        else:
+            return {"value": item}
+test_value = random.randint(1,100)
+mixed_list = ["100", 3.87, "True", 50, "hello", test_value]
+processed = {"numeric": [], "text": [], "other": []}
+unique_types = set()
+for item in mixed_list:
+    if isinstance(item, str):
+        try:
+            item = float(item) if '.' in item else int(item)
+        except ValueError:
+            pass
+    category = classify_type(item)
+    result = cast_and_operate(item)
+    processed[category].append(result)
+    unique_types.add(type(item).__name__)
+unique_types_fs = frozenset(unique_types)
+print("\n Processed Results: ")
+for key, value in processed.items():
+    print(f"{key.title()}: ", value)
+print("\nUnique types (frozenset): ", unique_types_fs)
+print("Timestamp: ", datetime.now()) 
